4f1df44d796cb481f4e86ce22dc30e26a164e763
ElenaGavrina <45360118+ElenaGavrina@users.noreply.github.com>
2023-05-15 19:36:36 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,36 +1,26 @@
 package main
 
-import(
+import (
 	"fmt"
-
+	"log"
+	"bufio"
+	"os"
+	
+	"github.com/mnogu/go-calculator"
 )
 
-func main(){
-	var num1, num2 int
-	var op string
+func main() {
+	fmt.Println("Enter the data to calculate: ")	
+	
+	reader := bufio.NewReader(os.Stdin)
+	val, err := reader.ReadString('\n')
+	if err != nil {
+    	    fmt.Println(err)
+  	}
 
-	fmt.Print("Enter the first number: ")
-	fmt.Scanln(&num1)
-	fmt.Print("Enter the second number: ")
-	fmt.Scanln(&num2)
-	fmt.Print("Enter the operator ( + - * / ): ")
-	fmt.Scanln(&op)
-
-	switch op {
-	case "+":
-		fmt.Printf("%d %s %d = %d",num1,op,num2,num1 + num2)
-	case "-":
-		fmt.Printf("%d %s %d = %d",num1,op,num2,num1 - num2)
-	case "*":
-		fmt.Printf("%d %s %d = %d",num1,op,num2,num1 * num2)
-	case "/":
-		if num2 == 0 {
-			fmt.Println("Divide by Zero situation")
-		}else{
-			fmt.Printf("%d %s %d = %d",num1,op,num2,num1 / num2)
-		}
-	default:
-		fmt.Println("Invalid operator")
+	result, err:= calculator.Calculate(val)
+	if err != nil {
+		log.Fatal(err)
 	}
-
+	fmt.Println(result)
 }
