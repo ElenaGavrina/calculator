a439809d9516b9578e836f0c54ebeb3f23ae6142
ElenaGavrina <45360118+ElenaGavrina@users.noreply.github.com>
2024-02-02 05:16:41 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,26 +1,46 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"bufio"
-	"os"
-	
-	"github.com/mnogu/go-calculator"
-)
+import "fmt"
+
+func add(a, b int) int {
+	return a + b
+}
+
+func sub(a, b int) int {
+	return a - b
+}
+
+func mult(a, b int) int {
+	return a * b
+}
+
+func div(a, b int) (c int) {
+	return a / b
+}
 
 func main() {
-	fmt.Println("Enter the data to calculate: ")	
-	
-	reader := bufio.NewReader(os.Stdin)
-	val, err := reader.ReadString('\n')
-	if err != nil {
-    	    fmt.Println(err)
-  	}
-
-	result, err:= calculator.Calculate(val)
-	if err != nil {
-		log.Fatal(err)
+	var a int
+	var b int
+	var c string
+	fmt.Println("Введите первое число:")
+	fmt.Scanln(&a)
+	fmt.Println("Введите второе число:")
+	fmt.Scanln(&b)
+	fmt.Println("Введите действие: + для сложения, - для вычитания, * для умножения, / для деления")
+	fmt.Scanln(&c)
+	switch{
+	case c == "+":
+		fmt.Println(add(a, b))
+	case c == "-":
+		fmt.Println(sub(a, b))
+	case c == "*":
+		fmt.Println(mult(a,b))
+	case c == "/":
+		if b == 0{
+			fmt.Println("Делить на ноль нельзя!")
+			return
+		}
+		fmt.Println(div(a,b))
 	}
-	fmt.Println(result)
+
 }
